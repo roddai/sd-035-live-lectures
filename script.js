9836c148adb60c65b532ed73de88ed6cb71def23
Rodrigo <roddai@hotmail.com>
2023-07-04 10:34:43 -0300

    cria o código para os tamanhos de fraldas

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ let num1 = 35;
 let num2 = '35';
 
 if (num1 === num2) {
-  console.log(true);
+  // console.log(true);
 } else {
-  console.log(false);
+  // console.log(false);
 }
 
 // = é atribuição (exemplo x = 4);
@@ -12,3 +12,28 @@ if (num1 === num2) {
 // === é comparação estrita (aqui importa o tipo de dado - exemplo 35 === 35, a resposta é true). (35 === '35', a resposta é false)
 
 // ====================================
+
+let fralda;
+let babyPeso = 'Eddy';
+
+if (typeof babyPeso !== 'number') {
+  fralda = 'Insira valor númerico para o peso do bebê.'
+} else if (babyPeso <= 4) {
+  fralda = 'RN';
+} else if (babyPeso > 4 && babyPeso <= 6) {
+  fralda = 'RN+';
+} else if (babyPeso > 6 && babyPeso <= 8) {
+  fralda = 'P';
+} else if (babyPeso > 8 && babyPeso <= 10) {
+  fralda = 'M';
+} else if (babyPeso > 10 && babyPeso <= 13) {
+  fralda = 'G';
+} else if (babyPeso > 13 && babyPeso <= 15) {
+  fralda = 'XG';
+} else if (babyPeso > 15 && babyPeso <= 20) {
+  fralda = 'XXG';
+} else {
+  fralda = 'Já está bom, né!';
+}
+
+console.log(fralda);
