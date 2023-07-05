caff58214d76031174f7886aff03923b59a3685d
Rodrigo <roddai@hotmail.com>
2023-07-05 17:36:10 -0300

    exemplo com array

---
+++ lottery-checker.js
@@ -0,0 +1,12 @@
+const lotteryChecker = [34, 43, 23, 56, 28, 12];
+
+console.log(lotteryChecker);
+
+console.log(lotteryChecker[0]);
+console.log(lotteryChecker[1]);
+console.log(lotteryChecker[2]);
+console.log(lotteryChecker[3]);
+console.log(lotteryChecker[4]);
+console.log(lotteryChecker[5]);
+
+console.log('O comprimento do array é: ' + lotteryChecker.length);
