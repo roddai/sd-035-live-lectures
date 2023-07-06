47724dda236fbe2a08fb58637dfac1d1f11d0eff
Rodrigo <roddai@hotmail.com>
2023-07-06 20:25:45 -0300

    exemplo biggestSmallerNumber

---
+++ biggestSmallerNumber.js
@@ -0,0 +1,17 @@
+function biggestSmallerNumber (array) {
+  let bigger = array[0];
+  let smaller = array[0];
+  
+  for (let index = 1; index < array.length; index += 1) {
+    if (array[index] > bigger) {
+      bigger = array[index];
+    } else if (array[index] < smaller) {
+      smaller = array[index];
+    }
+  }
+  
+  return `Menor: ${smaller} ----> Maior: ${bigger}`;
+}
+
+const arrayNumbers = [1, 2, 6, 8, 10, 4, 5, -78, 11];
+console.log(biggestSmallerNumber(arrayNumbers));
