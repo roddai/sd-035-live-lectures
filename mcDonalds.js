f6ebe8d3812e4fae3e646f0b62f9baa6b40b030b
Rodrigo <roddai@hotmail.com>
2023-07-09 18:38:49 -0300

    cria o arquivo mcDonalds.js e adiciona um objeto

---
+++ mcDonalds.js
@@ -0,0 +1,10 @@
+const mcDonalds = {
+  bigMac: 26.90,
+  bigTasty: 39.90,
+  mcFritasMedia: 13.90,
+  topSundayChocolate: 11.90,
+  isDelicious: true,
+  city: 'São Paulo',
+  unit: 'Shopping Morumbi',
+};
+
