0287c385eadce4593d5e5834e394c41269b3949b
Rodrigo <roddai@hotmail.com>
2023-08-15 18:16:57 -0300

    instala o jest

---
+++ data.js
@@ -0,0 +1,46 @@
+const harryPotterMovies = {
+  filmes: [
+    {
+      "nome": "Harry Potter e a Pedra Filosofal", 
+      "ano_lancamento": 2001, 
+      "atores_principais": ["Daniel Radcliffe", "Emma Watson", "Rupert Grint"]
+    },
+    {
+      "nome": "Harry Potter e a Câmara Secreta",
+      "ano_lancamento": 2002,
+      "atores_principais": ["Daniel Radcliffe", "Emma Watson", "Rupert Grint"]
+    },
+    {
+      "nome": "Harry Potter e o Prisioneiro de Azkaban",
+      "ano_lancamento": 2004,
+      "atores_principais": ["Daniel Radcliffe", "Emma Watson", "Rupert Grint"]
+    },
+    {
+      "nome": "Harry Potter e o Cálice de Fogo",
+      "ano_lancamento": 2005,
+      "atores_principais": ["Daniel Radcliffe", "Emma Watson", "Rupert Grint"]
+    },
+    {
+      "nome": "Harry Potter e a Ordem da Fênix",
+      "ano_lancamento": 2007,
+      "atores_principais": ["Daniel Radcliffe", "Emma Watson", "Rupert Grint"]
+    },
+    {
+      "nome": "Harry Potter e o Enigma do Príncipe",
+      "ano_lancamento": 2009,
+      "atores_principais": ["Daniel Radcliffe", "Emma Watson", "Rupert Grint"]
+    },
+    {
+      "nome": "Harry Potter e as Relíquias da Morte - Parte 1",
+      "ano_lancamento": 2010,
+      "atores_principais": ["Daniel Radcliffe", "Emma Watson", "Rupert Grint"]
+    },
+    {
+      "nome": "Harry Potter e as Relíquias da Morte - Parte 2",
+      "ano_lancamento": 2011,
+      "atores_principais": ["Daniel Radcliffe", "Emma Watson", "Rupert Grint"]
+    }
+  ]
+}
+
+module.exports = harryPotterMovies;
--- script.js
+++ script.js
@@ -1,47 +1,4 @@
-const harryPotterMovies = {
-  filmes: [
-    {
-      "nome": "Harry Potter e a Pedra Filosofal", 
-      "ano_lancamento": 2001, 
-      "atores_principais": ["Daniel Radcliffe", "Emma Watson", "Rupert Grint"]
-    },
-    {
-      "nome": "Harry Potter e a Câmara Secreta",
-      "ano_lancamento": 2002,
-      "atores_principais": ["Daniel Radcliffe", "Emma Watson", "Rupert Grint"]
-    },
-    {
-      "nome": "Harry Potter e o Prisioneiro de Azkaban",
-      "ano_lancamento": 2004,
-      "atores_principais": ["Daniel Radcliffe", "Emma Watson", "Rupert Grint"]
-    },
-    {
-      "nome": "Harry Potter e o Cálice de Fogo",
-      "ano_lancamento": 2005,
-      "atores_principais": ["Daniel Radcliffe", "Emma Watson", "Rupert Grint"]
-    },
-    {
-      "nome": "Harry Potter e a Ordem da Fênix",
-      "ano_lancamento": 2007,
-      "atores_principais": ["Daniel Radcliffe", "Emma Watson", "Rupert Grint"]
-    },
-    {
-      "nome": "Harry Potter e o Enigma do Príncipe",
-      "ano_lancamento": 2009,
-      "atores_principais": ["Daniel Radcliffe", "Emma Watson", "Rupert Grint"]
-    },
-    {
-      "nome": "Harry Potter e as Relíquias da Morte - Parte 1",
-      "ano_lancamento": 2010,
-      "atores_principais": ["Daniel Radcliffe", "Emma Watson", "Rupert Grint"]
-    },
-    {
-      "nome": "Harry Potter e as Relíquias da Morte - Parte 2",
-      "ano_lancamento": 2011,
-      "atores_principais": ["Daniel Radcliffe", "Emma Watson", "Rupert Grint"]
-    }
-  ]
-}
+const harryPotterMovies = require("./data");
 
 // FUNÇÃO QUE FORNECE TÍTULO DO FILME OU UM ERRO
 const getHarryPotterMovie = (year) => {
@@ -82,7 +39,7 @@ const addHarryPotterMovie = (year) => {
 // console.log(addHarryPotterMovie('xablau'));
 // console.log(addHarryPotterMovie());
 
-console.log(getHarryPotterMovie(2010));
+// console.log(getHarryPotterMovie(2010));
 // console.log(getHarryPotterMovie(2015));
 // console.log(getHarryPotterMovie('xablau'));
 
