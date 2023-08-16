158a548988eed882ad38d16b56fcead877aec794
Rodrigo <roddai@hotmail.com>
2023-08-16 17:44:50 -0300

    implementa os testes da função findClubByYear

--- tests/data.spec.js
+++ tests/data.spec.js
@@ -2,26 +2,32 @@ const data = require('../src/data');
 
 describe('Testa a base de dados.', () => {
   it('Verifica se a base de dados é um objeto.', () => {
-    expect();
+    fail('Teste vazio!');
+    // expect();
   });
 
   it('Verifica se a base de dados possui 30 times.', () => {
-    expect();
+    fail('Teste vazio!');
+    // expect();
   });
 
   it('Verifica se a base de dados possui o time "Manchester United".', () => {
-    expect();
+    fail('Teste vazio!');
+    // expect();
   });
 
   it('Verifica se todos os times possuem a propriedade "name".', () => {
-    expect();
+    fail('Teste vazio!');
+    // expect();
   });
 
   it('Verifica se, ao tentar buscar o time na posição 928 do array, retorna undefined.', () => {
-    expect();
+    fail('Teste vazio!');
+    // expect();
   });
 
   it('Verifica se existe algum time que o nome começa com uma determinada letra.', () => {
-    expect();
+    fail('Teste vazio!');
+    // expect();
   });
 });
--- tests/findClubByYear.test.js
+++ tests/findClubByYear.test.js
@@ -5,7 +5,8 @@ describe('Testes da função findClubByYear', () => {
     expect(typeof findClubByYear).toBe('function');
   });
 
-  it('Verifica se a função findClubByYear, ao receber 2015 como parâmetro, retorna "Barcelona".', () => {
-    expect();
+  it('Verifica se a função findClubByYear, ao receber 2015 como parâmetro, retorna "O campeão mundial do ano 2015 foi o Barcelona".', () => {
+    // fail('Teste vazio!');
+    expect(findClubByYear(2015)).toBe("O campeão mundial do ano 2015 foi o Barcelona");
   });
 });
