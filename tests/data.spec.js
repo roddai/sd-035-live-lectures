62559a36c46c5b3044065a7f5df6186a208b5d17
Rodrigo <roddai@hotmail.com>
2023-08-16 17:13:15 -0300

    setup inicial da aula 6.3

---
+++ tests/data.spec.js
@@ -0,0 +1,27 @@
+const data = require('../src/data');
+
+describe('Testa a base de dados.', () => {
+  it('Verifica se a base de dados é um objeto.', () => {
+    expect();
+  });
+
+  it('Verifica se a base de dados possui 30 times.', () => {
+    expect();
+  });
+
+  it('Verifica se a base de dados possui o time "Manchester United".', () => {
+    expect();
+  });
+
+  it('Verifica se todos os times possuem a propriedade "name".', () => {
+    expect();
+  });
+
+  it('Verifica se, ao tentar buscar o time na posição 928 do array, retorna undefined.', () => {
+    expect();
+  });
+
+  it('Verifica se existe algum time que o nome começa com uma determinada letra.', () => {
+    expect();
+  });
+});
---
+++ tests/findClubByYear.test.js
@@ -0,0 +1,11 @@
+const { findClubByYear } = require('../src/findClubByYear');
+
+describe('Testes da função findClubByYear', () => {
+  it('Verifica se a função findClubByYear existe.', () => {
+    expect();
+  });
+
+  it('Verifica se a função findClubByYear, ao receber 2015 como parâmetro, retorna "Barcelona".', () => {
+    expect();
+  });
+});
