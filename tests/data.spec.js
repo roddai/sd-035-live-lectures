58da230c895c5c4ff3b7d7978d709d43b04e8ece
Rodrigo <roddai@hotmail.com>
2023-08-16 19:04:35 -0300

    adiciona teste para a base data

--- tests/data.spec.js
+++ tests/data.spec.js
@@ -2,32 +2,51 @@ const data = require('../src/data');
 
 describe('Testa a base de dados.', () => {
   it('Verifica se a base de dados é um objeto.', () => {
-    fail('Teste vazio!');
-    // expect();
+    expect(typeof data).toBe('object');
   });
 
   it('Verifica se a base de dados possui 30 times.', () => {
-    fail('Teste vazio!');
-    // expect();
+    expect(data.clubs).toHaveLength(30);
   });
 
   it('Verifica se a base de dados possui o time "Manchester United".', () => {
-    fail('Teste vazio!');
-    // expect();
+    const objectManchester = {
+      name: 'Manchester United',
+      country: 'Inglaterra',
+      years: [1999, 2008],
+      logo: 'https://upload.wikimedia.org/wikipedia/en/thumb/7/7a/Manchester_United_FC_crest.svg/640px-Manchester_United_FC_crest.svg.png',
+    }
+    expect(data.clubs).toContainEqual(objectManchester);
   });
 
   it('Verifica se todos os times possuem a propriedade "name".', () => {
-    fail('Teste vazio!');
-    // expect();
+    for (let obj of data.clubs) {
+      expect(obj).toHaveProperty('name');
+    }
   });
 
   it('Verifica se, ao tentar buscar o time na posição 928 do array, retorna undefined.', () => {
-    fail('Teste vazio!');
-    // expect();
+    expect(data.clubs[928]).toBeUndefined();
+  });
+
+  it('Verifica se, ao tentar buscar o time na posição 9 do array, não retorna undefined.', () => {
+    expect(data.clubs[9]).not.toBeUndefined();
   });
 
   it('Verifica se existe algum time que o nome começa com uma determinada letra.', () => {
-    fail('Teste vazio!');
-    // expect();
+    const verifyFirstLetter = (letter) => {
+      let foundTeamName;
+
+      for (let index = 0; index < data.clubs.length; index += 1) {
+        if (data.clubs[index].name[0] === letter) {
+          return foundTeamName = data.clubs[index].name;
+        }
+      }
+      return `Time não encontrado`;
+    }
+    expect(verifyFirstLetter('M')).toMatch(/^M/);
+    expect(verifyFirstLetter('A')).toMatch(/^A/);
+    expect(verifyFirstLetter('S')).toMatch(/^S/);
+    expect(verifyFirstLetter('C')).toMatch(/^C/);
   });
 });
