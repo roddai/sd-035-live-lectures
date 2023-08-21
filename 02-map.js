b61a54e0dd488f405883b56917486265206adcef
Rodrigo <roddai@hotmail.com>
2023-08-21 11:11:59 -0300

    Resolve o exercício 2

--- 02-map.js
+++ 02-map.js
@@ -7,3 +7,29 @@ const birds = [
   { id: 'SP11', name: 'Old World Sparrow', type: 'Sparrow' },
   { id: 'DV2', name: 'Mourning Dove', type: 'Dove' },
 ];
+
+// USANDO FOR
+const birdsNameWithFor = (birds) => {
+  const myArray = [];
+
+  for (let bird of birds) {
+    const birdName = bird.name.toUpperCase();
+    myArray.push(`${birdName}`);
+  }
+
+  return myArray;
+}
+
+console.log(birdsNameWithFor(birds));
+
+// USANDO MAP
+// const birdsName = (birds) => {
+//   return birds.map( (bird) => {
+//     return bird.name.toUpperCase();
+//   } )
+// }
+
+// Refatorando
+const birdsName = (birds) => birds.map( (bird) => bird.name.toUpperCase());
+
+console.log(birdsName(birds));
