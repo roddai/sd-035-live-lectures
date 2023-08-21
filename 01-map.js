458e73dfb52970f1069b13225366f69a40cc3606
Rodrigo <roddai@hotmail.com>
2023-08-21 11:03:13 -0300

    Resolve o exercício 1

--- 01-map.js
+++ 01-map.js
@@ -1,5 +1,6 @@
 // 1. Transforme (Mapeie) o array de objetos cities em um array de strings com o seguinte formato:
 // a) "${nomeDaCidade} - ${siglaDoEstado}"
+// Exemplo: "Manaus - AM"
 
 const cities = [
   { state: "AM", name: "Manaus" , region: "N" },
@@ -12,3 +13,29 @@ const cities = [
   { state: "RN", name: "Touros", region: "NE" },
   { state: "CE", name: "Jericoacoara", region: "NE" }
 ];
+
+// USANDO FOR
+const citiesAndStatesWithFor = (cities) => {
+  const myArray = [];
+
+  for (let index = 0; index < cities.length; index += 1) {
+    myArray.push(`${cities[index].name} - ${cities[index].state}`);
+  }
+
+  return myArray;
+}
+
+console.log(citiesAndStatesWithFor(cities));
+
+// USANDO MAP
+// const citiesAndStatesWithMap = (cities) => {
+//   return cities.map( (city) => {
+//     return `${city.name} - ${city.state}`;
+//   });
+// }
+
+// Refatorando
+const citiesAndStatesWithMap = (cities) => cities.map( (city) => `${city.name} - ${city.state}`);
+
+console.log(citiesAndStatesWithMap(cities));
+// console.table(citiesAndStatesWithMap(cities));
