696ac2e14a6537232dba38b9d645863cc9ce8095
Rodrigo <roddai@hotmail.com>
2023-08-22 18:59:54 -0300

    resolve o exemplo 5

--- 05-filter.js
+++ 05-filter.js
@@ -7,3 +7,44 @@ const users = [
   { firstName: 'Lisa', lastName: 'Simpson', isDriver: false },
   { firstName: 'Maggie', lastName: 'Simpson', isDriver: false }
 ]
+
+// USANDO FOR
+const filterCanTDriveWithFor = (users) => {
+  const canTDrive = [];
+
+  for (let user of users) {
+    if (user.isDriver === false) {
+      canTDrive.push(user);
+    }
+  }
+  return canTDrive;
+}
+
+// console.log(filterCanTDriveWithFor(users));
+
+// USANDO FILTER()
+// const filterCanTDrive = (users) => {
+//   return users.filter((user) => {
+//     return user.isDriver === false;
+//   })
+// }
+
+// Refatorando
+// const filterCanTDrive = (users) => users.filter((user) => user.isDriver === false);
+
+// TRAZER SOMENTE OS NOMES DENTRO DO ARRAY
+// const filterCanTDrive = (users) => {
+//   const result = users.filter((user) => {
+//     return user.isDriver === false;
+//   })
+//   return result.map((object) => {
+//     return object.firstName; 
+//   });
+// }
+
+// Refatorando
+const filterCanTDrive = (users) => users
+  .filter((user) => user.isDriver === false)
+  .map((object) => object.firstName);
+
+console.log(filterCanTDrive(users));
